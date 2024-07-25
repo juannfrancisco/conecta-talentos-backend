b6245c590165a877ac35acbc9a22afa5245934d1
Juan <juan.maldonado.leon@gmail.com>
2024-07-25 19:03:24 -0400

    Modelo creado

---
+++ src/models/empresa.ts
@@ -0,0 +1,8 @@
+export class Empresa {
+  constructor(
+    public id: string,
+    public nombre: string,
+    public sitioWeb: string,
+    public tipo: string,
+  ) {}
+}
---
+++ src/models/postulacion.ts
@@ -0,0 +1,10 @@
+import { Estudiante } from './estudiante';
+
+export class Postulacion {
+  constructor(
+    public id: number,
+    public fechaPostulacion: Date,
+    public estudiante: Estudiante,
+    public estado: string,
+  ) {}
+}
